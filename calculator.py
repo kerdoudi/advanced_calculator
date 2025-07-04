d113ab3909b5193e263e3f1650d3130b48306982
Kerdoudi Lamine <lamine.kerdoudi@gmail.com>
2025-07-04 11:06:08 +0100

    Add subtraction function

--- calculator.py
+++ calculator.py
@@ -1,7 +1,11 @@
 # calculator.py
 
+def subtract(a, b):
+    return a - b
+
 def main():
     print("Welcome to the calculator")
+    print("5 - 2 =", subtract(5, 2))
 
 if __name__ == "__main__":
     main()
