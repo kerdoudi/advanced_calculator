ddaae1860e1b32279a395fa006d4e01343d876db
Kerdoudi Lamine <lamine.kerdoudi@gmail.com>
2025-07-04 11:04:36 +0100

    Add addition function

--- calculator.py
+++ calculator.py
@@ -1,7 +1,11 @@
 # calculator.py
 
+def add(a, b):
+    return a + b
+
 def main():
     print("Welcome to the calculator")
+    print("2 + 3 =", add(2, 3))
 
 if __name__ == "__main__":
     main()
